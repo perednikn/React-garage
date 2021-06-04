954687814623e6082792e45aa2df1b663be6299f
Nicolas Perednik <perednikn@gmail.com>
2021-06-04 11:39:23 -0300

    Desafío Contador con botón

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,18 +1,24 @@
 import React, { useState } from 'react';
 
-function ItemCount({ stock, initial}) {
+function ItemCount({ stock, initial, onAdd}) {
  const [cantidad, setCantidad] = useState(initial);
    
  let stockActual = stock;
 
+
+
+
     const addItems = () => {
         if (cantidad < stockActual){
             setCantidad(cantidad + 1);
+            onAdd();
         }}
 
     const restaItems = () => {
         cantidad >= 1 && setCantidad(cantidad - 1);
+        onAdd();
         }
+        
 
 
     /* useState(initial); */
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -2,14 +2,19 @@ import ItemCount from './ItemCount.js'
 
 
 function ItemList(){
+  const onAdd = () => {
+    console.log("ha modificado el carrito");
+  }
+  
   return <div className="fichaProducto">
+    
     <h3 className="fotoProducto"> UNA FOTO</h3>
     <h5 className="nombreProducto">NombreProducto</h5>
     <h6 className="detalleProducto">Detalle del Producto<br />
         Detalle del Producto<br />
         Detalle del Producto</h6>
 
-        <ItemCount  stock={15} initial={11} onAdd/>
+        <ItemCount  stock={15} initial={11} onAdd={onAdd}/>
         <button type="button" className="btn btn-success">Agregar a Carrito</button>
   </div>
 
